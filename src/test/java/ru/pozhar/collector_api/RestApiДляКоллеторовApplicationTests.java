3e8d35adb21619ec5336329ce650257bb6060d51
Pozhar <abrakatabr@mail.ru>
2025-08-19 18:19:58 +0300

    Перый коммит

---
+++ src/test/java/ru/pozhar/collector_api/RestApiДляКоллеторовApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pozhar.collector_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiДляКоллеторовApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
