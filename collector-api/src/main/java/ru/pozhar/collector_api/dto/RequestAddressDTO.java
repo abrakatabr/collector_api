3208cd3d98b43e3339728c7acbe0ca8ab6f24dff
Pozhar <abrakatabr@mail.ru>
2025-09-18 22:33:04 +0300

    Добавил возможность работать дебагером в кубере, начал заполнять файл openapi

--- collector-api/src/main/java/ru/pozhar/collector_api/dto/RequestAddressDTO.java
+++ collector-api/src/main/java/ru/pozhar/collector_api/dto/RequestAddressDTO.java
@@ -1,7 +1,6 @@
 package ru.pozhar.collector_api.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record RequestAddressDTO(
